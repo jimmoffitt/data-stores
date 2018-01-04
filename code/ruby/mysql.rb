03c967de16265d826ab62dabd1a2bd68db9d92a4
@snowman <jimmoffitt@yahoo.com>
2018-01-04 08:14:45 -0700

    Create mysql.rb

---
+++ code/ruby/mysql.rb
@@ -0,0 +1,20 @@
+'''
+NOTES:
+Database Object. Singleton by intent, not yet by design.
+
+One option is to use (Rails) ActiveRecord for data management, but it seems that may abstract away more than
+desired.
+
+This class is meant to demonstrate basic code for building a "database" class for use with Twitter API clients that retrieve Tweets of interest. It is written in Ruby, but in its present form hopefully will
+read like pseudo-code for other languages.
+'''
+
+
+
+
+
+
+
+
+
+
