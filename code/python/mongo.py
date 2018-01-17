dc40879ae4c64aa4d0bf2e1ee7734d4ed82384af
@snowman <jimmoffitt@yahoo.com>
2018-01-17 09:36:57 -0700

    Update mongo.py

--- code/python/mongo.py
+++ code/python/mongo.py
@@ -4,16 +4,17 @@ from pprint import pprint
 
 print "started"
 
-#Create a MongoDB connection... Mongo service instance must be running. (>>>sudo mongod)
 client = MongoClient()
-#client = MongoClient('localhost', 27017) #Pick a style, any style
+
+#Pick a style, any style
+#client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb://localhost:27017')
 
-#Specify a data store, creating it if it does not exist. 
-#db = client.tweets #Pick a style, any style
+#Connect to data store
+#db = client.tweets
 db = client['tweets']
 
-#Connect to JSON document collection, creating it if it does not exist. 
+#Specify collection
 tweets_test = db.tweets_test
 
 #Load up and iterate through some Tweets -- this file is a single response from Search API (~500 Tweets in "results" array)
@@ -23,5 +24,10 @@ tweets = tweet_data['results']
 
 for tweet in tweets:
     result = tweets_test.insert_one(tweet)
+    
+    
+#Code for retrieving documents
+tweet_of_interest = tweets.find_one({'user.handle': 'snowman'})
+print(tweet_of_interest)
 
 print "Quitting"
