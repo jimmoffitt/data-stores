7b5353442b4d5f380887c5be797e413ec56d94de
@snowman <jimmoffitt@yahoo.com>
2018-01-18 23:12:18 -0700

    Update mongo_transform.py

--- code/python/mongo_transform.py
+++ code/python/mongo_transform.py
@@ -18,40 +18,4 @@ def transform_tweet_json(tweet)
 end
 
 
-def process_tweet(tweet):
 
-
-
- #pre-processing 
- #Sort out which Tweet message and entities attributes to grab (depends on whether this is an extended Tweet or legacy size)
-
- message = ""
- entities = {}
-
-  
-  objects = {}
-  
-  try:
-    tweet_object = {
-      'tweet_id': tweet['id'].split(':')[2],
-      'message': text,
-      'screen_name': tweet['actor']['preferredUsername'],
-      'posted_at': parse(tweet['created_at']),
-      'gnip_rules': tweet['gnip']['matching_rules']
-    }
-    
-    objects['tweet'] = tweet_object
-    
-    user_object = {
-      'user_id': tweet['user']['id']   #Or use 'id_str'?
-      
-    }
-    
-    objects['user'] = user_object
-    
-    
-
-    return objects
-
-  except KeyError, e:
-    return False
